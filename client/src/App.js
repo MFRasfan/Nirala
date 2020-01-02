feba0e73766af6a24fb1b140bdb3d749e93ffdda
MFRasfan <rasfanfaiz@gmail.com>
2020-01-02 15:35:16 -0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <div class="collection">
-    <a href="#!" class="collection-item"><span class="badge">1</span>Alan</a>
-    <a href="#!" class="collection-item"><span class="new badge">6</span>Alan</a>
-    <a href="#!" class="collection-item">Alan</a>
-    <a href="#!" class="collection-item"><span class="badge">14</span>Alan</a>
-  </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
