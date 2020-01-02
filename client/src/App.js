7da901a05b4c85c43bdeb0cb399894b9da7f1c1d
MFRasfan <rasfanfaiz@gmail.com>
2020-01-02 15:34:41 -0500

    new files

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div class="collection">
+    <a href="#!" class="collection-item"><span class="badge">1</span>Alan</a>
+    <a href="#!" class="collection-item"><span class="new badge">6</span>Alan</a>
+    <a href="#!" class="collection-item">Alan</a>
+    <a href="#!" class="collection-item"><span class="badge">14</span>Alan</a>
+  </div>
+    </div>
+  );
+}
+
+export default App;
