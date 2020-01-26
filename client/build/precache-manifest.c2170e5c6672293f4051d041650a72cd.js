e0c82a3ae681ddb87e3067dc4d68d1c7783c4dae
MFRasfan <rasfanfaiz@gmail.com>
2020-01-26 00:00:20 -0500

    packages

---
+++ client/build/precache-manifest.c2170e5c6672293f4051d041650a72cd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e0d3273341a000907f6295515dfefd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31b09f86ad1df44ca959",
+    "url": "/static/css/2.246d0aaf.chunk.css"
+  },
+  {
+    "revision": "5097e54e7cf561d574b1",
+    "url": "/static/css/main.5924bb92.chunk.css"
+  },
+  {
+    "revision": "31b09f86ad1df44ca959",
+    "url": "/static/js/2.d3bf9a80.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d3bf9a80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5097e54e7cf561d574b1",
+    "url": "/static/js/main.aac12335.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "769feb2119156dc06f4157da053802d6",
+    "url": "/static/media/blog-img-1.769feb21.jpg"
+  },
+  {
+    "revision": "65bcbc899f379216109acd0b6c494618",
+    "url": "/static/media/fontawesome-webfont.65bcbc89.svg"
+  },
+  {
+    "revision": "7149833697a959306ec3012a8588dcfa",
+    "url": "/static/media/fontawesome-webfont.71498336.eot"
+  },
+  {
+    "revision": "c4668ed2440df82d3fd2f8be9d31d07d",
+    "url": "/static/media/fontawesome-webfont.c4668ed2.ttf"
+  },
+  {
+    "revision": "d95d6f5d5ab7cfefd09651800b69bd54",
+    "url": "/static/media/fontawesome-webfont.d95d6f5d.woff"
+  }
+]);
